6f061d11b1151ea61cead1779411b0ba1180a0ad
AliAlShah <96441735+AliAlShah@users.noreply.github.com>
2022-05-11 19:54:01 +0300

    Manipulated data
    Splitted data into train and test groups
    Created model

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ from sklearn.metrics import classification_report
 
 data = pd.read_csv("https://raw.githubusercontent.com/laxmimerit/twitter-data/master/twitter30k_cleaned.csv")
 
+tfid = TfidfVectorizer(max_features=10000, ngram_range=(1, 2))
+x = data["twitts"]
+x = tfid.fit_transform(x)
+y = data["sentiment"]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=0)
+
+model = LinearSVC()
+model.fit(x_train, y_train)
+
